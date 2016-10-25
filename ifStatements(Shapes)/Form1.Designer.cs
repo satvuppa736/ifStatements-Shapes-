1d44d000d2baa9a7bc050c3788b3621a9bba2b9b
Home <Home@UPPALHOME>
2016-10-25 15:45:33 -0400

    Commit (DONE) October 25 2016

--- ifStatements(Shapes)/Form1.Designer.cs
+++ ifStatements(Shapes)/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.introLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.shapeInput = new System.Windows.Forms.TextBox();
+            this.colourInput = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // introLabel
+            // 
+            this.introLabel.AutoSize = true;
+            this.introLabel.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.introLabel.Location = new System.Drawing.Point(19, 51);
+            this.introLabel.Name = "introLabel";
+            this.introLabel.Size = new System.Drawing.Size(584, 28);
+            this.introLabel.TabIndex = 0;
+            this.introLabel.Text = "Enter \"r\" for Red, \"g\" for Green, or \"b\" for Blue";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter \"circle\" or \"square\"";
+            // 
+            // shapeInput
+            // 
+            this.shapeInput.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Bold);
+            this.shapeInput.Location = new System.Drawing.Point(386, 178);
+            this.shapeInput.Name = "shapeInput";
+            this.shapeInput.Size = new System.Drawing.Size(278, 34);
+            this.shapeInput.TabIndex = 2;
+            this.shapeInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // colourInput
+            // 
+            this.colourInput.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Bold);
+            this.colourInput.Location = new System.Drawing.Point(638, 51);
+            this.colourInput.Name = "colourInput";
+            this.colourInput.Size = new System.Drawing.Size(109, 34);
+            this.colourInput.TabIndex = 3;
+            this.colourInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Stencil", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(594, 233);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(153, 62);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "BEGIN";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.HotPink;
+            this.ClientSize = new System.Drawing.Size(817, 521);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.colourInput);
+            this.Controls.Add(this.shapeInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.introLabel);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label introLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox shapeInput;
+        private System.Windows.Forms.TextBox colourInput;
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- ifStatements(Shapes)/Form1.cs
+++ ifStatements(Shapes)/Form1.cs
@@ -16,5 +16,52 @@ namespace ifStatements_Shapes_
         {
             InitializeComponent();
         }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Red, 12);
+            Graphics fg = this.CreateGraphics();
+
+            char colour = Convert.ToChar(colourInput.Text);
+            string shape = shapeInput.Text;
+
+            if (colour == 'g' && shape == "circle")
+            {
+                fg.Clear(Color.HotPink);
+                pen.Color = Color.Green;
+                fg.DrawEllipse(pen, 200, 300, 100, 100);
+            }
+            if (colour == 'r' && shape == "circle")
+            {
+                fg.Clear(Color.HotPink);
+                pen.Color = Color.Red;
+                fg.DrawEllipse(pen, 200, 300, 100, 100);
+            }
+            if (colour == 'b' && shape == "circle")
+            {
+                fg.Clear(Color.HotPink);
+                pen.Color = Color.Blue;
+                fg.DrawEllipse(pen, 200, 300, 100, 100);
+            }
+
+            if (colour == 'g' && shape == "square")
+            {
+                fg.Clear(Color.HotPink);
+                pen.Color = Color.Green;
+                fg.DrawRectangle(pen, 200, 300, 100, 100);
+            }
+            if (colour == 'r' && shape == "square")
+            {
+                fg.Clear(Color.HotPink);
+                pen.Color = Color.Red;
+                fg.DrawRectangle(pen, 200, 300, 100, 100);
+            }
+            if (colour == 'b' && shape == "square")
+            {
+                fg.Clear(Color.HotPink);
+                pen.Color = Color.Blue;
+                fg.DrawRectangle(pen, 200, 300, 100, 100);
+            }
+        }
     }
 }
